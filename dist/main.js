9a25c69fb29f3ff89199f481dc5d8582d9960e86
gzver <gzveriachvili@gmail.com>
2022-01-31 12:37:22 +0200

    First push

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const content = document.querySelector('#content');
+const testPara = document.createElement('p');
+
+testPara.textContent = 'Tamada Georgian Restaurant';
+
+content.appendChild(testPara);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBjb250ZW50ID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignI2NvbnRlbnQnKTtcclxuY29uc3QgdGVzdFBhcmEgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdwJyk7XHJcblxyXG50ZXN0UGFyYS50ZXh0Q29udGVudCA9ICdUYW1hZGEgR2VvcmdpYW4gUmVzdGF1cmFudCc7XHJcblxyXG5jb250ZW50LmFwcGVuZENoaWxkKHRlc3RQYXJhKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const content = document.querySelector('#content');
+const testPara = document.createElement('p');
+
+testPara.textContent = 'Tamada Georgian Restaurant';
+
+content.appendChild(testPara);
